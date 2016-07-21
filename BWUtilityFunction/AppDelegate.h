d7cd896a70d4bc457acdfe43b8a875bc475ab97b
WangBo <wangbo@bluemoon.com.cn>
2016-07-21 15:30:11 +0800

    Upload Project

---
+++ BWUtilityFunction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BWUtilityFunction
+//
+//  Created by Bob Wong on 16/7/21.
+//  Copyright © 2016年 Bob Wong Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BWUtilityFunction/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BWUtilityFunction
+//
+//  Created by Bob Wong on 16/7/21.
+//  Copyright © 2016年 Bob Wong Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
